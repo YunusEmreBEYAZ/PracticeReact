201c88d2360b3614567542e5d706c7c3bb1a8303
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-07-18 23:54:01 +0200

    progress

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 7, 17);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 300.00;
     return (
         <div className="expense-item">
-            <div>18 July 2023</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2 >Car Insurance</h2>
-                <div className="expense-item__price">$300.00</div>
+                <h2 >{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     )
