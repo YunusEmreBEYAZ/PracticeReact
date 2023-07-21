d1bb9c2ae8a88eeba3bd998d9af23ba977934b8e
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-07-22 01:43:33 +0200

    Expenses component and some styling

---
+++ src/components/Expenses.js
@@ -0,0 +1,17 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css"
+
+function Expenses(props) {
+    return (
+        <div className="expenses">
+            {props.expenses.map((expense) => (
+                <ExpenseItem
+                    title={expense.title}
+                    date={expense.date}
+                    amount={expense.amount} />
+            ))}
+        </div>
+    )
+}
+
+export default Expenses;
